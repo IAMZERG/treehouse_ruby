fe6e61d7eb4772ef3f0ba66b9e1610c7a7d7b6e9
Wesley Wright <inventor487@gmail.com>
2015-09-09 13:14:53 -0400

    First part of Ruby Loops completed

---
+++ while_loop.rb
@@ -0,0 +1,5 @@
+answer = ""
+while answer == "Y"
+        print "do you want me to repeat this pointless loop again? (Y/n) "
+        answer = gets.chomp
+end
---
+++ while_number.rb
@@ -0,0 +1,20 @@
+def print_hello(number_of_times)
+        i = 0
+        while i < number_of_times
+                puts "hello"
+                i += 1
+        end
+end
+
+answer = ""
+while answer.to_i < 5
+        print "How many times do you want to print hello? (Enter a number > 5 to exit)."
+        answer = gets.chomp.to_i
+        if answer < 5 
+                print_hello(answer)
+        else
+                puts "Exiting"
+        end
+end
+print_hello(answer)
+
